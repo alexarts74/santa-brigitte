09064f6c921318548ba3744ae8ab6651aac00de8
alexandre artus <artusalexandre@yahoo.fr>
2023-06-21 11:45:05 +0200

    fix one bug controller rooms done and add view for all

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -15,8 +15,8 @@ class RoomsController < ApplicationController
 
   def create
     @room = Room.new(room_params)
-    room.user = current_user
-    if @clothe.save
+    @room.user = current_user
+    if @room.save
       redirect_to rooms_path
     else
       render :new
@@ -34,6 +34,12 @@ class RoomsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def destroy
+    @room = Room.find(params[:id])
+    @room.destroy
+    redirect_to rooms_path
   end
 
   private
