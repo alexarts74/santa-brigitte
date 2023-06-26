67acb08e503f609082445f9137c8688f6b491c29
alexandre artus <artusalexandre@yahoo.fr>
2023-06-26 10:09:15 +0200

    mise en place du formulaire de contact par mail

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,34 +1,44 @@
 class ReservationsController < ApplicationController
 
-  def new
-    @reservation = Reservation.new
+  def contact
   end
 
-  def create
-    @reservation = Reservation.new(reservation_params)
-    #mettre en place le systeme de date
-
-    @reservation.user = current_user
-    #probleme avec l'envoie d'un mail et le systeme de paiement vont'il ensemble
-    if @reservation.save
-      # ReservationMailer.with(reservation: @reservation).reservation_email.deliver_now
-
-      redirect_to root_path
-      # flash[:success] = "Your message has been sent successfully."
-    else
-      render :new
-    end
-
-    # Perform any necessary actions with the form data
-
-  end
 end
 
-private
 
-def reservation_params
-  params.require(:reservation).permit(:name, :lastname, :email, :message)
-end
+
+
+
+
+#   def new
+#     @reservation = Reservation.new
+#   end
+
+#   def create
+#     @reservation = Reservation.new(reservation_params)
+#     #mettre en place le systeme de date
+
+#     @reservation.user = current_user
+#     #probleme avec l'envoie d'un mail et le systeme de paiement vont'il ensemble
+#     if @reservation.save
+#       # ReservationMailer.with(reservation: @reservation).reservation_email.deliver_now
+
+#       redirect_to root_path
+#       # flash[:success] = "Your message has been sent successfully."
+#     else
+#       render :new
+#     end
+
+#     # Perform any necessary actions with the form data
+
+#   end
+# end
+
+# private
+
+# def reservation_params
+#   params.require(:reservation).permit(:name, :lastname, :email, :message)
+# end
 
  # def create
   #   @tree = Tree.find(params[:tree_id])
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,8 @@ Rails.application.routes.draw do
   root to: "pages#home"
   resources :rooms
   resources :reservations, only: [:new, :create, :destroy]
-  resources :reservations, only: %i[new create]
 
 
   get '/my_account', to: 'users#my_account'
-  # get '/contact', to: 'reservations#create'
+  get '/contact', to: 'reservations#contact'
 end
