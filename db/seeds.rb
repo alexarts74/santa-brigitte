b2f611cce99903c7d695197b02594eef9c59036e
alexandre artus <artusalexandre@yahoo.fr>
2023-07-08 14:16:41 +0200

    add some front on home and index

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,21 +20,17 @@ puts "room is creating ..."
 
 Room.destroy_all
 
-room = Room.create!(name: 'Room n°1', description: 'Très belle chambre au bord du lac avec lit double et petit déjeuner compris dans le prix', price: 40, user: user)
-room_image = Cloudinary::Uploader.upload(Rails.root.join("app/assets/images/chambre_1.jpeg"))
-room.image.attach(io: URI.open(room_image['secure_url']), filename: "chambre_1.jpeg", content_type: "image/jpeg")
+room = Room.create!(name: 'Stanza del ritorno', description: 'Très belle chambre au bord du lac avec lit double et petit déjeuner compris dans le prix', price: 40, user: user)
+room_image = Cloudinary::Uploader.upload(Rails.root.join("app/assets/images/Stanza del ritorno.jpeg"))
+room.image.attach(io: URI.open(room_image['secure_url']), filename: "Stanza del ritorno.jpeg", content_type: "image/jpeg")
 
-room = Room.create!(name: 'Room n°2', description: 'Très belle chambre au bord du lac avec lit double et petit déjeuner compris dans le prix', price: 40, user: user)
-room_image = Cloudinary::Uploader.upload(Rails.root.join("app/assets/images/chambre_2.jpeg"))
-room.image.attach(io: URI.open(room_image['secure_url']), filename: "chambre_2.jpeg", content_type: "image/jpeg")
+room = Room.create!(name: 'Stanza della Signora', description: 'Très belle chambre au bord du lac avec lit double et petit déjeuner compris dans le prix', price: 40, user: user)
+room_image = Cloudinary::Uploader.upload(Rails.root.join("app/assets/images/Stanza della Signora.jpeg"))
+room.image.attach(io: URI.open(room_image['secure_url']), filename: "Stanza della Signora.jpeg", content_type: "image/jpeg")
 
-room = Room.create!(name: 'Room n°3', description: 'Très belle chambre au bord du lac avec lit double et petit déjeuner compris dans le prix', price: 40, user: user)
-room_image = Cloudinary::Uploader.upload(Rails.root.join("app/assets/images/chambre_3.jpeg"))
-room.image.attach(io: URI.open(room_image['secure_url']), filename: "chambre_3.jpeg", content_type: "image/jpeg")
-
-room = Room.create!(name: 'Room n°4', description: 'Très belle chambre au bord du lac avec lit double et petit déjeuner compris dans le prix', price: 40, user: user)
-room_image = Cloudinary::Uploader.upload(Rails.root.join("app/assets/images/chambre_4.jpeg"))
-room.image.attach(io: URI.open(room_image['secure_url']), filename: "chambre_4.jpeg", content_type: "image/jpeg")
+room = Room.create!(name: 'Stanza del Rossàl', description: 'Très belle chambre au bord du lac avec lit double et petit déjeuner compris dans le prix', price: 40, user: user)
+room_image = Cloudinary::Uploader.upload(Rails.root.join("app/assets/images/Stanza del Rossàl.jpeg"))
+room.image.attach(io: URI.open(room_image['secure_url']), filename: "Stanza del Rossàl.jpeg", content_type: "image/jpeg")
 
 puts "rooms created"
 puts "end of seed"
